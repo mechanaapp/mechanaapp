f62e50b64a69c3aad64f4adafe99decd613c10d2
Fernando Ochoa <ochoa.imt@gmail.cocm>
2025-04-27 12:12:44 -0500

    update home page content

--- src/App.js
+++ src/App.js
@@ -4,21 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <h1>Welcome to Mechana 🚗🛠️</h1>
+  <p>Find trusted car mechanics near you.</p>
+</div>
   );
 }
 
